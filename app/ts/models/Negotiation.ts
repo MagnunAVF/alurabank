b6afffb0d3a2780dd5e81e4ac2de7872835ddd8e
Magnun A V F <magnunavf@gmail.com>
2020-01-06 22:58:26 -0300

    Refactor Negotiation Model with syntactic sugar

--- app/ts/models/Negotiation.ts
+++ app/ts/models/Negotiation.ts
@@ -1,13 +1,8 @@
 class Negotiation {
-    private _date: Date;
-    private _quantity: number;
-    private _value: number;
-
-    constructor(date: Date, quantity: number, value: number) {
-        this._date = date;
-        this._quantity = quantity;
-        this._value = value;
-    }
+    constructor(
+        private _date: Date,
+        private _quantity: number,
+        private _value: number) { }
 
     get date() {
         return this._date;
