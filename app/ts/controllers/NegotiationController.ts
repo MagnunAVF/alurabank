5161d54b5e0f120935c808eb13b20589a149bd03
Magnun A V F <magnunavf@gmail.com>
2020-01-09 00:03:51 -0300

    Create Negotiations View

--- app/ts/controllers/NegotiationController.ts
+++ app/ts/controllers/NegotiationController.ts
@@ -3,11 +3,13 @@ class NegotiationController {
     private _inputQuantity: HTMLInputElement;
     private _inputValue: HTMLInputElement;
     private _negotiations = new Negotiations();
+    private _negotiationsView = new NegotiationsView('#negotiationsView');
 
     constructor() {
         this._inputDate = <HTMLInputElement>document.querySelector('#data');
         this._inputQuantity = <HTMLInputElement>document.querySelector('#quantidade');
         this._inputValue = <HTMLInputElement>document.querySelector('#valor');
+        this._negotiationsView.update();
     }
 
     add(event: Event) {
